e8610e2804d630999131ba0590dfae40ff09d295
hctrung2k4 <hctrung2k4@gmail.com>
2025-07-24 20:11:11 +0700

    fix order status

--- Services/Implementations/OrderDetailService.cs
+++ Services/Implementations/OrderDetailService.cs
@@ -61,7 +61,8 @@ namespace Services.Implementations
 
                 // Tìm status nhỏ nhất
                 var minStatus = allDetails.Min(d => (int)d.Status);
-
+                if (minStatus != 0)
+                    minStatus--;
                 // Cập nhật trạng thái order cha
                 var order = await _unitOfWork.OrderRepository
                     .FirstOrDefaultAsync(o => o.Id == detail.OrderId);
